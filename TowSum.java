2603ab5b196fbbfeb88b85ad24adea23c757ff5a
Pankaj <pankaj9057595572@gmail.com>
2024-04-16 23:15:59 +0530

    update function

--- TowSum.java
+++ TowSum.java
@@ -24,7 +24,19 @@ Output: [0,1]
 
 public class TowSum {
 
-    public int[] twoSum(int[] nums, int target) {
+    public static void main(String[] args){
+
+        int nums[] = {1,2,3,4,5};
+        int target = 8;
+
+        int[] arr = twoSum(nums,target);
+
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
         for(int i=0; i<nums.length; i++) {
             for(int j=i+1; j<nums.length; j++) {
                 if(nums[i] + nums[j] == target) {
